4fa6bb56cc7bfb9e5a407d04e3892042ef29fc48
Maksym Horbachevsky <maximgorb@gmail.com>
2023-11-19 20:37:16 +0200

    Added map to a list of concepts

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,16 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
-              {...CORE_CONCEPTS[2]}
-            />
-            <CoreConcept
-              {...CORE_CONCEPTS[3]}
-            />
+            {
+              CORE_CONCEPTS.map((concept) => {
+                return (
+                  <CoreConcept
+                    {...concept}
+                    key={concept.title}
+                  />
+                );
+              })
+            }
           </ul>
         </section>
         <section id="examples">
